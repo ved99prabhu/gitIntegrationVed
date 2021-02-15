ee3b868fe1ea795ade421eab81c90b66337646f1
ved99prabhu <78725668+ved99prabhu@users.noreply.github.com>
2021-02-16 00:59:03 +0530

    Add files via upload

---
+++ EXP23.py
@@ -0,0 +1,11 @@
+# •	Python Program to Convert Decimal to Binary, Octal and Hexadecimal(use function)
+class Conversion:
+	def Boh(self):
+		n=int(56)
+		print("The Binary equivalent is:",bin(n))
+		print("The Octal equivalent is:",oct(n))
+		print("The Hexadecimal equivalent is:",hex(n))
+s=Conversion()
+s.Boh()
+
+
